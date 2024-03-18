bb68ee1ad987b39bd0197841345bc9a59a993209
Clement Couturier <ccouturier@b2pweb.com>
2024-03-18 10:54:07 +0100

    [-] Suppression d'un nav link

--- PakistanLaravel11/resources/views/layouts/navigation.blade.php
+++ PakistanLaravel11/resources/views/layouts/navigation.blade.php
@@ -15,9 +15,6 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('refetch-events')" :active="request()->routeIs('refetch-events')">
-                        {{ __('Calendar') }}
-                    </x-nav-link>
                 </div>
             </div>
 
