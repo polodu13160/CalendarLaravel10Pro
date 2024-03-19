82f44b79df373b52cef5e5dfc2b1f3a85fffff8e
Clement Couturier <ccouturier@b2pweb.com>
2024-03-19 11:04:27 +0100

    [+] JQuery et Alpine sont maintenant compatibles

--- PakistanLaravel11/resources/js/app.js
+++ PakistanLaravel11/resources/js/app.js
@@ -5,3 +5,14 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+document.addEventListener("alpine:init", () => {
+    Alpine.setListenerManipulators(
+        (target, event, handler, options, modifiers) => {
+            jQuery(target).on(event, handler);
+        },
+        (target, event, handler, options) => {
+            jQuery(target).off(event, handler);
+        }
+    );
+});
